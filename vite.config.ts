b636494db9d50a0b5eb9a8d9026af31e3bfc2078
Kazumi Inada <hello@nandenjin.com>
2025-05-16 01:28:59 +0900

    feat: Add image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 70,
+        progressive: true,
+      },
+    }),
+  ],
   base: process.env.BASE_URL || "/",
 });
