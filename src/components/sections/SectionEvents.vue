a19bb598a979e8de8ffe1f92070068a54be15976
Kazumi Inada <hello@nandenjin.com>
2025-05-17 01:11:00 +0900

    feat: Add description for special event

--- src/components/sections/SectionEvents.vue
+++ src/components/sections/SectionEvents.vue
@@ -7,8 +7,15 @@ import TextSection from "../TextSection.vue";
     <h2>イベント</h2>
     <h3>オープニングパーティー</h3>
     <time datetime="2025-05-29 17:00:00">5/29（木） 17:00-</time>
-    <h3>トークイベント</h3>
+    <h3>トークイベント「Drawing Room」</h3>
     <time datetime="2025-06-07 14:00:00">6/20（土） 18:00-19:00</time>
+    <p>
+      「drawing」には「描く」だけでなく、「応接」という意味もあるそうです。本トークイベントでは、ジャグリングのような手つきで対話を重ねながら、今回の展示テーマやこれまでの作品とのつながりを掘り下げていきます。
+    </p>
+    <p>
+      例えば、そもそもなぜフォークロアに興味を持ったのか？そうした、本展示にも繋がる菅野のこれまでの作品をめぐる問いを、友人でジャグリングパフォーマーである結城敬介と共に掘り下げていきます。そこから生まれる作品のさらなる余白をめざして、トークイベント「Drawing
+      Room」を開催します。
+    </p>
     <p>
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLSdY4hVIGKlqpGQhAKquVG52v4hs7oogAXHaVL_ghRqoekwqRw/viewform"
@@ -39,6 +46,7 @@ import TextSection from "../TextSection.vue";
   grid-template-columns: auto 1fr;
   gap: 2rem;
   margin: 2em 0;
+  font-size: 0.9rem;
 
   @media screen and (max-width: 800px) {
     grid-template-columns: 1fr;
