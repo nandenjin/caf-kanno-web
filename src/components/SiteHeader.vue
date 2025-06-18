f98e3341b1e74a20811e610491ddd1791e61e81d
Kazumi Inada <hello@nandenjin.com>
2025-06-18 23:04:38 +0900

    feat: Use dark theme only for header (#20)
    * Revert "feat: Change color theme to dark (#15)"
    
    This reverts commit df9e4d0fce9f1d52071cbd9714ea04013742e747.
    
    * feat: Use dark theme only for header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -24,20 +24,24 @@ import PrinterClock from "./PrinterClock.vue";
   left: 0;
   bottom: 0;
   right: 0;
+  z-index: 0;
   margin: auto;
   height: 3rem;
   font-size: 3rem;
+  filter: invert(1);
 }
 
 header {
   position: relative;
   text-align: center;
   height: 100vh;
+  background-color: #111;
 
   &::after {
     content: "";
     display: block;
     position: absolute;
+    z-index: 2;
     bottom: calc(2rem + 3.5vmax);
     left: 0;
     right: 0;
@@ -47,7 +51,7 @@ header {
     border: 0.15rem solid white;
     border-style: none none solid solid;
     transform: rotate(-45deg) translateX(50%);
-    animation: jump 1s ease-in-out infinite;
+    animation: jump 1s ease-in-out infinite, lazy-in 1s ease-in-out 4s both;
   }
 }
 
@@ -73,6 +77,7 @@ header {
   height: 100svh;
   margin: auto;
   object-fit: cover;
+  z-index: 1;
   animation: lazy-in 1s ease-in-out 2.5s both;
 }
 
@@ -94,6 +99,7 @@ header {
   color: white;
   font-size: 2vw;
   animation: lazy-in 1s ease-in-out 3s both;
+  z-index: 2;
 
   @media screen and (max-width: 800px) {
     font-size: 4.5vw;
