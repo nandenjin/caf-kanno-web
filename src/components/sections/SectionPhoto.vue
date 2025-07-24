4f3ab274fe1f6cb0dd7ce14a2146cc1f66d7dc05
Kazumi Inada <hello@nandenjin.com>
2025-07-24 12:42:31 +0900

    fix: Make sizes of photos inline (#30)

--- src/components/sections/SectionPhoto.vue
+++ src/components/sections/SectionPhoto.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import TextSection from "../TextSection.vue";
+
+const sizes = "(max-width: 400px) 100vw, 800px";
+const style = {
+  width: "100%",
+  maxWidth: "100vw",
+};
 </script>
 
 <template>
@@ -13,7 +19,8 @@ import TextSection from "../TextSection.vue";
         ../../assets/photos/top_image.jpg?format=webp&w=600 600w,
         ../../assets/photos/top_image.jpg?format=webp&w=800 800w
       "
-      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+      :sizes="sizes"
+      :style="style"
     />
     <img
       src="../../assets/photos/0803.jpg?format=webp&w=400"
@@ -24,7 +31,8 @@ import TextSection from "../TextSection.vue";
         ../../assets/photos/0803.jpg?format=webp&w=600 600w,
         ../../assets/photos/0803.jpg?format=webp&w=800 800w
       "
-      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+      :sizes="sizes"
+      :style="style"
     />
     <img
       src="../../assets/photos/0806.jpg?format=webp&w=400"
@@ -35,7 +43,8 @@ import TextSection from "../TextSection.vue";
         ../../assets/photos/0806.jpg?format=webp&w=600 600w,
         ../../assets/photos/0806.jpg?format=webp&w=800 800w
       "
-      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+      :sizes="sizes"
+      :style="style"
     />
     <img
       src="../../assets/photos/0828.jpg?format=webp&w=400"
@@ -46,7 +55,8 @@ import TextSection from "../TextSection.vue";
         ../../assets/photos/0828.jpg?format=webp&w=600 600w,
         ../../assets/photos/0828.jpg?format=webp&w=800 800w
       "
-      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+      :sizes="sizes"
+      :style="style"
     />
     <img
       src="../../assets/photos/0834.jpg?format=webp&w=400"
@@ -57,7 +67,8 @@ import TextSection from "../TextSection.vue";
         ../../assets/photos/0834.jpg?format=webp&w=600 600w,
         ../../assets/photos/0834.jpg?format=webp&w=800 800w
       "
-      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+      :sizes="sizes"
+      :style="style"
     />
     <img
       src="../../assets/photos/0841.jpg?format=webp&w=400"
@@ -68,14 +79,14 @@ import TextSection from "../TextSection.vue";
         ../../assets/photos/0841.jpg?format=webp&w=600 600w,
         ../../assets/photos/0841.jpg?format=webp&w=800 800w
       "
-      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+      :sizes="sizes"
+      :style="style"
     />
   </TextSection>
 </template>
 
 <style scoped>
 img {
-  width: 100%;
   height: auto;
   margin: 0.5rem 0;
 }
