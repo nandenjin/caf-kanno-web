cb6483f617a5efb5f684e486d661d3321bba674c
Kazumi Inada <hello@nandenjin.com>
2025-07-24 12:05:13 +0900

    fix: Replace photos (#27)

--- src/components/sections/SectionPhoto.vue
+++ src/components/sections/SectionPhoto.vue
@@ -4,71 +4,72 @@ import TextSection from "../TextSection.vue";
 
 <template>
   <TextSection>
-    <img src="../../assets/photos/0751.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0751.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0751.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0751.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0758.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0758.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0758.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0758.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0763.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0763.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0763.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0763.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0768.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0768.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0768.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0768.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0776.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0776.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0776.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0776.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0777.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0777.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0777.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0777.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0783.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0783.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0783.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0783.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0800.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0800.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0800.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0800.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0803.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0803.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0803.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0803.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0806.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0806.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0806.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0806.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0828.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0828.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0828.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0828.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0834.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0834.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0834.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0834.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
-    <img src="../../assets/photos/0841.jpg?format=webp&w=400" alt="" 
-         srcset="../../assets/photos/0841.jpg?format=webp&w=400 400w, 
-                 ../../assets/photos/0841.jpg?format=webp&w=600 600w, 
-                 ../../assets/photos/0841.jpg?format=webp&w=800 800w"
-         sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px" />
+    <img
+      src="../../assets/photos/top_image.jpg?format=webp&w=400"
+      alt=""
+      loading="lazy"
+      srcset="
+        ../../assets/photos/top_image.jpg?format=webp&w=400 400w,
+        ../../assets/photos/top_image.jpg?format=webp&w=600 600w,
+        ../../assets/photos/top_image.jpg?format=webp&w=800 800w
+      "
+      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+    />
+    <img
+      src="../../assets/photos/0803.jpg?format=webp&w=400"
+      alt=""
+      loading="lazy"
+      srcset="
+        ../../assets/photos/0803.jpg?format=webp&w=400 400w,
+        ../../assets/photos/0803.jpg?format=webp&w=600 600w,
+        ../../assets/photos/0803.jpg?format=webp&w=800 800w
+      "
+      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+    />
+    <img
+      src="../../assets/photos/0806.jpg?format=webp&w=400"
+      alt=""
+      loading="lazy"
+      srcset="
+        ../../assets/photos/0806.jpg?format=webp&w=400 400w,
+        ../../assets/photos/0806.jpg?format=webp&w=600 600w,
+        ../../assets/photos/0806.jpg?format=webp&w=800 800w
+      "
+      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+    />
+    <img
+      src="../../assets/photos/0828.jpg?format=webp&w=400"
+      alt=""
+      loading="lazy"
+      srcset="
+        ../../assets/photos/0828.jpg?format=webp&w=400 400w,
+        ../../assets/photos/0828.jpg?format=webp&w=600 600w,
+        ../../assets/photos/0828.jpg?format=webp&w=800 800w
+      "
+      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+    />
+    <img
+      src="../../assets/photos/0834.jpg?format=webp&w=400"
+      alt=""
+      loading="lazy"
+      srcset="
+        ../../assets/photos/0834.jpg?format=webp&w=400 400w,
+        ../../assets/photos/0834.jpg?format=webp&w=600 600w,
+        ../../assets/photos/0834.jpg?format=webp&w=800 800w
+      "
+      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+    />
+    <img
+      src="../../assets/photos/0841.jpg?format=webp&w=400"
+      alt=""
+      loading="lazy"
+      srcset="
+        ../../assets/photos/0841.jpg?format=webp&w=400 400w,
+        ../../assets/photos/0841.jpg?format=webp&w=600 600w,
+        ../../assets/photos/0841.jpg?format=webp&w=800 800w
+      "
+      sizes="(min-width: 1024px) 800px, (min-width: 768px) 600px, 400px"
+    />
   </TextSection>
 </template>
 
